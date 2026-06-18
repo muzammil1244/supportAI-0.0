e9e22b163e6ea2b8c615b06dff7eeba14bfbc9ff
Muzammil <muzammil844641@gmail.com>
2026-06-18 09:58:02 +0530

    added start.sh content change

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-uv run celery -A src.celery_app worker --loglevel=info &
+uv run celery -A src.celery.celery_app worker --loglevel=info &
 uv run uvicorn main:app --host 0.0.0.0 --port $PORT
