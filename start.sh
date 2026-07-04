91c138719aa07660015a3b0b89877092202fe446
Muzammil <muzammil844641@gmail.com>
2026-07-04 16:35:05 +0530

    sh file edited worker load dicreas

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-uv run celery -A src.celery.celery_app worker --loglevel=info &
+uv run celery -A src.celery.celery_app worker --loglevel=info --concurrency=1 --pool=prefork &
 uv run uvicorn main:app --host 0.0.0.0 --port $PORT
